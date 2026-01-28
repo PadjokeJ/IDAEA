cc9a9852f93526f938d4ff10940ecd28ba412da3
PadjokeJ <jonatan@padjokej.dev>
2026-01-28 16:30:56 +0100

    add login style

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import flask_login
 
 from dotenv import load_dotenv
@@ -54,6 +54,7 @@ def login():
 def logout():
   flask_login.logout_user()
   return redirect("/login")
+
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=True)
